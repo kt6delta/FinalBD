10b3fb950c175d8e6c72644d632905c40323bb49
Destilou <dmcuestaf@correo.udistrital.edu.co>
2023-11-25 18:50:17 -0500

    contacto

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,6 +2,16 @@
 import Contacto from './Contacto.vue';
 import Direcciones from './Direcciones.vue';
 export default {
+  async beforeMount() {
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/tipodoc`);
+      const data = await response.json();
+      this.tipoDoc = data
+
+    } catch (error) {
+    }
+  },
   data() {
     return {
       //atributos del formulario
@@ -14,7 +24,8 @@ export default {
       Name: "",
       Apellido: "",
       nDoc: "",
-      tipoDoc: "",
+      tipoDoc: [],
+      tipoDocPh:''
     };
   },
   methods: {
@@ -102,10 +113,9 @@ export default {
           <div class="row">
             <div class="col-sm-8">
               <div class="form-group">
-                <span class="form-label">Tipo usuario</span>
-                <select id="tipos" class="form-control" name="tipoDoc"  v-model="TipeUser">
-                  <option value="cliente">cliente</option>
-                  <option value="provedor">provedor</option>
+                <span class="form-label">Tipo de Persona</span>
+                <select id="tipos" class="form-control" name="tipoDoc" v-model="TipeUser">
+                  <option v-for="i in tipoDoc" :value="i[1]">{{ i[1] }}</option>
                 </select>
                 <span class="select-arrow"></span>
               </div>
@@ -126,15 +136,14 @@ export default {
             <div class="col-sm-6">
               <div class="form-group">
                 <span class="form-label">Nombre</span>
-                <input class="form-control" type="text" placeholder="Nombre" name="nombre"  v-model="Name">
+                <input class="form-control" type="text" placeholder="Nombre" name="nombre" v-model="Name">
               </div>
             </div>
             <!--ingresar info de Apellido-->
             <div class="col-sm-6">
               <div class="form-group">
                 <span class="form-label">Apellido</span>
-                <input class="form-control" type="text" placeholder="Apellido" name="apellido" 
-                  v-model="Apellido">
+                <input class="form-control" type="text" placeholder="Apellido" name="apellido" v-model="Apellido">
               </div>
             </div>
           </div>
@@ -143,17 +152,15 @@ export default {
             <div class="col-sm-8">
               <div class="form-group">
                 <span class="form-label">Numero de documento</span>
-                <input class="form-control" type="text" name="nDoc" placeholder="Numero de documento" 
-                  v-model="nDoc">
+                <input class="form-control" type="text" name="nDoc" :placeholder="`Numero de ${tipoDocPh}`" v-model="nDoc">
               </div>
             </div>
             <!--ingresar info de Tipo-->
             <div class="col-sm-4">
               <div class="form-group">
                 <span class="form-label">Tipo</span>
-                <select id="tipos" class="form-control" name="tipoDoc"  v-model="tipoDoc">
-                  <option value="id">id</option>
-                  <option value="cc">cc</option>
+                <select id="tipos" class="form-control" name="tipoDoc" v-model="tipoDocPh">
+                  <option v-for="i in tipoDoc" :value="i[1]">{{ i[1] }}</option>
                 </select>
                 <span class="select-arrow"></span>
               </div>
@@ -189,105 +196,111 @@ p#text {
 
 /* color del form*/
 .booking-form {
-    background-color: #fff;
-    -webkit-box-shadow: 0 5px 20px -5px rgba(0, 0, 0, .3);
-    box-shadow: 0 5px 20px -5px rgba(0, 0, 0, .3);
-    border-radius: 4px
+  background-color: #fff;
+  -webkit-box-shadow: 0 5px 20px -5px rgba(0, 0, 0, .3);
+  box-shadow: 0 5px 20px -5px rgba(0, 0, 0, .3);
+  border-radius: 4px
 }
+
 /* btn del form*/
 .booking-form .form-group {
-    position: relative;
-    margin-bottom: 30px
+  position: relative;
+  margin-bottom: 30px
 }
+
 /* marco externo form*/
 .booking-form .form-control {
-    background-color: #ebecee;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    color: #3e485c;
-    font-size: 14px
+  background-color: #ebecee;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  color: #3e485c;
+  font-size: 14px
 }
 
 /* inputs form*/
 .booking-form .form-control::-webkit-input-placeholder {
-    color: rgba(62, 72, 92, .3)
+  color: rgba(62, 72, 92, .3)
 }
 
 .booking-form .form-control:-ms-input-placeholder {
-    color: rgba(62, 72, 92, .3)
+  color: rgba(62, 72, 92, .3)
 }
 
 .booking-form .form-control::placeholder {
-    color: rgba(62, 72, 92, .3)
+  color: rgba(62, 72, 92, .3)
 }
 
 .booking-form input[type=date].form-control:invalid {
-    color: rgba(62, 72, 92, .3)
+  color: rgba(62, 72, 92, .3)
 }
 
 .booking-form select.form-control {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none
 }
+
 /* flechas del select form*/
 .booking-form select.form-control+.select-arrow {
-    position: absolute;
-    right: 0;
-    bottom: 4px;
-    width: 32px;
-    line-height: 32px;
-    height: 32px;
-    text-align: center;
-    pointer-events: none;
-    color: rgba(62, 72, 92, .3);
-    font-size: 14px
+  position: absolute;
+  right: 0;
+  bottom: 4px;
+  width: 32px;
+  line-height: 32px;
+  height: 32px;
+  text-align: center;
+  pointer-events: none;
+  color: rgba(62, 72, 92, .3);
+  font-size: 14px
 }
 
 .booking-form select.form-control+.select-arrow:after {
-    content: '\279C';
-    display: block;
-    -webkit-transform: rotate(90deg);
-    transform: rotate(90deg)
+  content: '\279C';
+  display: block;
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg)
 }
+
 /* labels form*/
 .booking-form .form-label {
-    display: inline-block;
-    color: #3e485c;
-    font-weight: 700;
-    margin-bottom: 6px;
-    margin-left: 7px
+  display: inline-block;
+  color: #3e485c;
+  font-weight: 700;
+  margin-bottom: 6px;
+  margin-left: 7px
 }
+
 /* btn form*/
 .booking-form .submit-btn {
-    display: inline-block;
-    color: #fff;
-    background-color: #1e62d8;
-    font-weight: 700;
-    padding: 12px 25px;
-    border-radius: 4px;
-    border: none;
-    -webkit-transition: .2s all;
-    transition: .2s all
+  display: inline-block;
+  color: #fff;
+  background-color: #1e62d8;
+  font-weight: 700;
+  padding: 12px 25px;
+  border-radius: 4px;
+  border: none;
+  -webkit-transition: .2s all;
+  transition: .2s all
 }
 
 .booking-form .submit-btn:hover,
 .booking-form .submit-btn:focus {
-    opacity: .9
+  opacity: .9
 }
+
 /* titulo del form*/
 .booking-cta h1 {
-    font-size: 52px;
-    text-transform: uppercase;
-    color: #fff;
-    font-weight: 700
+  font-size: 52px;
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 700
 }
+
 /* text del form*/
 .booking-cta p {
-    font-size: 16px;
-    color: #fff;
-}
-</style>
+  font-size: 16px;
+  color: #fff;
+}</style>
